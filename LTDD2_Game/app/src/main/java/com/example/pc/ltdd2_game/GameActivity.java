0e5ac04d2fce894c70fda5ff75a41fca2947a6fb
kupivampire <kupivampire@gmail.com>
2018-03-20 19:58:43 +0700

    Up project lên github

---
+++ LTDD2_Game/app/src/main/java/com/example/pc/ltdd2_game/GameActivity.java
@@ -0,0 +1,13 @@
+package com.example.pc.ltdd2_game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_4in1_layout);
+    }
+}
