edcf2384dc44622b988dae39e478884bbd7eca27
dangvanphu <kupivampire@gmail.com>
2018-05-09 08:03:33 +0700

    Tạo màn hình chọn game + add thêm thư viện e3droi

---
+++ LTDD2_Game/app/src/main/java/com/example/pc/ltdd2_game/ChooseGameActivity.java
@@ -0,0 +1,13 @@
+package com.example.pc.ltdd2_game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choose_game_layout);
+    }
+}
