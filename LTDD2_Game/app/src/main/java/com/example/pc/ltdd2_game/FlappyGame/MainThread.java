a86b6dbcf9a2a15b9d8b47b0f302cb1d8c9f8e8a
dangvanphu <kupivampire@gmail.com>
2018-05-11 09:07:50 +0700

    Update FlappyBird Game

---
+++ LTDD2_Game/app/src/main/java/com/example/pc/ltdd2_game/FlappyGame/MainThread.java
@@ -0,0 +1,83 @@
+package com.example.pc.ltdd2_game.FlappyGame;
+
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+
+
+public class MainThread extends Thread {
+    private GameView gameView;
+    private SurfaceHolder surfaceHolder;
+    private boolean running;
+    public static Canvas canvas;
+    private int targetFPS = 30;
+    private double averageFPS;
+
+
+    public MainThread(SurfaceHolder surfaceHolder, GameView gameView) {
+
+
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.gameView = gameView;
+
+
+    }
+
+    @Override
+    public void run()
+    {
+
+        long startTime;
+        long timeMillis;
+        long waitTime;
+        long totalTime = 40;
+        int frameCount =0;
+        long targetTime = 1000/targetFPS;
+
+
+        while(running) {
+            startTime = System.nanoTime();
+            canvas = null;
+
+            try {
+                canvas = this.surfaceHolder.lockCanvas();
+                synchronized (surfaceHolder) {
+                    this.gameView.update();
+                    this.gameView.draw(canvas);
+                }
+            } catch (Exception e) {
+            }
+            finally{
+                if(canvas!=null)
+                {
+                    try {
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                    }
+                    catch(Exception e){e.printStackTrace();}
+                }
+            }
+
+            timeMillis = (System.nanoTime() - startTime) / 10;
+            waitTime = targetTime-timeMillis;
+
+            try{
+                this.sleep(waitTime);
+            }catch(Exception e){}
+
+            totalTime += System.nanoTime()-startTime;
+            frameCount++;
+            if(frameCount == targetFPS)
+            {
+                averageFPS = 10000/((totalTime/frameCount)/10);
+                frameCount =0;
+                totalTime = 0;
+                System.out.println(averageFPS);
+            }
+        }
+
+    }
+
+    public void setRunning(boolean isRunning) {
+        running = isRunning;
+    }
+}
